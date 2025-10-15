b99436ad9fb5bf4496af56713dc0368e8a014fd6
Andrew Sander <andrewsanderf@gmail.com>
2025-10-15 17:07:40 -0300

    Feat: Exercício das bandeiras

---
+++ ifpb exercises main src-pages_exercises_web-script-country-flags__codes_js_code/js/main.js
@@ -0,0 +1,13 @@
+import flags from './model/flags.js';
+
+const principal = document.querySelector("main");
+
+for (const flag of flags){
+    principal.insertAdjacentHTML(
+    "beforeend",
+    `<div class="flag col-2 my-2 text-center">
+    <img src="${flag.image}" alt="${flag.name}">
+    <p>${flag.name}</p>
+    </div>`
+    );
+}
